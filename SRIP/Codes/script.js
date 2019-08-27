1e3f7221fe738f2afce690384bbb3cfd393f8939
vinaytejab <vinaytejaemailid@gmail.com>
2019-08-27 08:43:11 +0530

    try exp commit

---
+++ SRIP/Codes/script.js
@@ -0,0 +1,294 @@
+var canvas = document.querySelector("canvas");
+var c = canvas.getContext("2d");
+canvas.width = window.innerWidth; //Setting the canvas to full width of the window
+canvas.height = window.innerHeight; //Setting the canvas to full height of the window
+
+function draw_main()
+{
+	c.beginPath();
+	
+	c.beginPath();
+	c.strokeStyle="black";
+	c.lineWidth="1";
+	c.moveTo(462,200);
+	c.lineTo(612,200);
+	c.lineTo(612,165);
+	c.lineTo(782,165);
+	c.lineTo(782,200);
+	c.lineTo(932,200);
+	c.lineTo(932,220);
+	c.lineTo(742,220);
+	c.lineTo(742,260);
+	c.lineTo(647,260);
+	c.lineTo(647,220);
+	c.lineTo(462,220);
+	c.lineTo(462,200);
+	c.fillStyle="#CCCCCC";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.strokeStyle="black";
+	c.lineWidth="1";
+	c.moveTo(475,200);
+	c.lineTo(475,189);
+	c.lineTo(505,189);
+	c.lineTo(505,200);
+	c.lineTo(475,200);
+	c.fillStyle="#999999";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.closePath();
+	c.beginPath();
+	c.strokeStyle="black";
+	c.lineWidth="1";
+	c.moveTo(919,200);
+	c.lineTo(919,189);
+	c.lineTo(889,189);
+	c.lineTo(889,200);
+	c.lineTo(919,200);
+	c.fillStyle="#999999";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.strokeStyle="black";
+	c.lineWidth="1";
+	c.moveTo(462,600);
+	c.lineTo(462,533);
+	c.lineTo(654,533);
+	c.lineTo(654,300);
+	c.lineTo(679.5,300);
+	c.lineTo(679.5,135);
+	c.lineTo(709.5,135);
+	c.lineTo(709.5,300);
+	c.lineTo(734.5,300);
+	c.lineTo(734.5,533);
+	c.lineTo(932,533);
+	c.lineTo(932,600);
+	c.lineTo(462,600);
+	c.fillStyle="#999999";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.arc(685,290,7.5,1.25*Math.PI,1.75*Math.PI);
+	c.stroke();
+	c.closePath();
+	c.beginPath();
+	c.arc(694,279,7.5,0.2*Math.PI,0.7*Math.PI);
+	c.stroke();
+	c.closePath();
+	c.beginPath();
+	c.arc(705,289,7.5,1.25*Math.PI,1.7*Math.PI);
+	c.lineTo(709.5,135);
+	c.lineTo(679.5,135);
+	c.lineTo(679.5,283);
+	c.fillStyle="#CCCCCC";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.strokeStyle="#0067FD";
+	c.fillStyle="#0067FD";
+	c.moveTo(432,560);
+	c.lineTo(688,560);
+	c.lineTo(688,500);
+	c.lineTo(694,500);
+	c.lineTo(694,566);
+	c.lineTo(432,566);
+	c.lineTo(432,560);
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.strokeStyle="#0067FD";
+	c.fillStyle="#0067FD";
+	c.moveTo(962,566);
+	c.lineTo(780,566);
+	c.lineTo(780,533);
+	c.lineTo(786,533);
+	c.lineTo(786,560);
+	c.lineTo(962,560);
+	c.lineTo(962,566);
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.strokeStyle="black";
+	c.lineWidth="1";
+	c.moveTo(482.5,187);
+	c.lineTo(482.5,600);
+	c.lineTo(497.5,600);
+	c.lineTo(497.5,187);
+	c.lineTo(482.5,187);
+	c.fillStyle="#CCCCCC";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+
+	c.beginPath();
+	c.strokeStyle="black";
+	c.lineWidth="1";
+	c.moveTo(911.5,187);
+	c.lineTo(911.5,600);
+	c.lineTo(896.5,600);
+	c.lineTo(896.5,187);
+	c.lineTo(911.5,187);
+	c.fillStyle="#CCCCCC";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.strokeStyle="black";
+	c.lineWidth="1";
+	c.moveTo(540,213);
+	c.lineTo(550,213);
+	c.lineTo(550,540);
+	c.lineTo(540,540);
+	c.lineTo(540,213);
+	c.fillStyle="white";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.strokeStyle="black";
+	c.lineWidth="1";
+	c.moveTo(840,213);
+	c.lineTo(850,213);
+	c.lineTo(850,540);
+	c.lineTo(840,540);
+	c.lineTo(840,213);
+	c.fillStyle="white";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.strokeStyle="black";
+	c.lineWidth="1";
+	c.moveTo(654,528);
+	c.lineTo(648,528);
+	c.lineTo(648,304);
+	c.lineTo(654,304);
+	c.lineTo(654,528);
+	c.fillStyle="white";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.strokeStyle="black";
+	c.lineWidth="1";
+	c.moveTo(741,528);
+	c.lineTo(735,528);
+	c.lineTo(735,304);
+	c.lineTo(741,304);
+	c.lineTo(741,528);
+	c.fillStyle="white";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.arc(645,515,2,0,2*Math.PI);
+	c.fillStyle="black";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.arc(645,506,2,0,2*Math.PI);
+	c.fillStyle="black";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.arc(645,320,2,0,2*Math.PI);
+	c.fillStyle="black";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.arc(645,329,2,0,2*Math.PI);
+	c.fillStyle="black";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+
+	c.beginPath();
+	c.arc(744,515,2,0,2*Math.PI);
+	c.fillStyle="black";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.arc(744,506,2,0,2*Math.PI);
+	c.fillStyle="black";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.arc(744,320,2,0,2*Math.PI);
+	c.fillStyle="black";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.arc(744,329,2,0,2*Math.PI);
+	c.fillStyle="black";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+
+	c.beginPath();
+	c.moveTo(654,490);
+	c.lineTo(654,500);
+	c.lineTo(734,500);
+	c.lineTo(734,490);
+	c.lineTo(654,490);
+	c.fillStyle="#FFCC00";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+
+	c.beginPath();
+	c.moveTo(654,340);
+	c.lineTo(654,350);
+	c.lineTo(734,350);
+	c.lineTo(734,340);
+	c.lineTo(654,340);
+	c.fillStyle="#FFCC00";
+	c.fill();
+	c.stroke();
+	c.closePath();
+
+	c.beginPath();
+	c.rect(654,350,80,140);
+	c.fillStyle="#984C2C";
+	c.fill();
+	c.stroke();
+
+	
+}
+draw_main();
+
